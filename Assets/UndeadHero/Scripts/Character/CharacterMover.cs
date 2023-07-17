e987e3052f1388b681a7e48c4bee6f9315aab32b
snezhniylis <noreply@snezhniylis.dev>
2023-07-17 20:10:40 +0300

    Implement character movement

---
+++ Assets/UndeadHero/Scripts/Character/CharacterMover.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UndeadHero.Services.Input;
+using UndeadHero.Infrastructure;
+
+namespace UndeadHero.Character {
+  [RequireComponent(typeof(CharacterController))]
+  public class CharacterMover : MonoBehaviour {
+    [SerializeField]
+    private CharacterController _characterController;
+    [SerializeField]
+    private float _movementSpeed = 4.0f;
+
+    private IInputService _inputService;
+    private Camera _camera;
+
+    private void OnValidate() {
+      _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Awake() {
+      _inputService = Game.InputService;
+      _camera = Camera.main;
+    }
+
+    private void Update() {
+      Vector3 movementVector = _inputService.MovementAxis;
+      if (movementVector.sqrMagnitude > Mathf.Epsilon) {
+        movementVector = _camera.transform.TransformDirection(movementVector);
+        movementVector.y = 0;
+        movementVector.Normalize();
+
+        transform.forward = movementVector;
+      }
+      else {
+        movementVector = Vector3.zero;
+      }
+
+      movementVector += Physics.gravity;
+      _characterController.Move(_movementSpeed * Time.deltaTime * movementVector);
+    }
+  }
+}
