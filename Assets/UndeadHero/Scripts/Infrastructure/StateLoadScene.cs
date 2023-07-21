dd4def90eff86fea860e6f4b841671462b78ec83
snezhniylis <noreply@snezhniylis.dev>
2023-07-21 18:38:19 +0300

    Dynamically load HUD and Doge prefabs

--- Assets/UndeadHero/Scripts/Infrastructure/StateLoadScene.cs
+++ Assets/UndeadHero/Scripts/Infrastructure/StateLoadScene.cs
@@ -1,5 +1,10 @@
+using UnityEngine;
+
 namespace UndeadHero.Infrastructure {
   class StateLoadScene : IStatePayloaded<string> {
+    private const string HudPrefabPath = "UI/HUD";
+    private const string DogePrefabPath = "Characters/Playable/Doge/Doge";
+
     private readonly GameStateMachine _gameStateMachine;
     private readonly SceneLoader _sceneLoader;
 
@@ -9,8 +14,18 @@ namespace UndeadHero.Infrastructure {
     }
 
     public void Enter(string sceneName) =>
-      _sceneLoader.Load(sceneName);
+      _sceneLoader.Load(sceneName, OnSceneLoaded);
 
     public void Exit() { }
+
+    private void OnSceneLoaded() {
+      InstantiatePrefab(HudPrefabPath);
+      InstantiatePrefab(DogePrefabPath);
+    }
+
+    private static GameObject InstantiatePrefab(string path) {
+      var prefab = Resources.Load<GameObject>(path);
+      return Object.Instantiate(prefab);
+    }
   }
 }
