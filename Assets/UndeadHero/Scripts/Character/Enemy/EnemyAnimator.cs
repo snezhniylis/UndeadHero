69192178fa96175592ff3660696e85e0ca855766
snezhniylis <noreply@snezhniylis.dev>
2023-08-02 14:56:13 +0300

    Make enemies automatically play movement animations when moving

--- Assets/UndeadHero/Scripts/Character/Enemy/EnemyAnimator.cs
+++ Assets/UndeadHero/Scripts/Character/Enemy/EnemyAnimator.cs
@@ -1,5 +1,26 @@
 using UndeadHero.Character.Animation;
+using UnityEngine;
+using UnityEngine.AI;
 
 namespace UndeadHero.Character.Enemy {
-  public class EnemyAnimator : CharacterAnimator { }
+  [RequireComponent(typeof(NavMeshAgent))]
+  public class EnemyAnimator : CharacterAnimator {
+    [SerializeField]
+    private NavMeshAgent _navMeshAgent;
+
+    protected override void OnValidate() {
+      base.OnValidate();
+      _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+      float enemySpeed = _navMeshAgent.velocity.magnitude;
+      if (enemySpeed > 0.01) {
+        Move(enemySpeed);
+      }
+      else {
+        StopMoving();
+      }
+    }
+  }
 }
