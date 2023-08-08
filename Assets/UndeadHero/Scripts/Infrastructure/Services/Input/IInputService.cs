efc3b121ce3916bc7208af280bebbf336b42d482
snezhniylis <noreply@snezhniylis.dev>
2023-08-08 10:48:42 +0300

    Add hero attack button

--- Assets/UndeadHero/Scripts/Infrastructure/Services/Input/IInputService.cs
+++ Assets/UndeadHero/Scripts/Infrastructure/Services/Input/IInputService.cs
@@ -4,5 +4,6 @@ using UndeadHero.Infrastructure.Services;
 namespace UndeadHero.Infrastructure.Services.Input {
   public interface IInputService : IService {
     Vector2 MovementAxis { get; }
+    bool AttackTriggered { get; }
   }
 }
--- Assets/UndeadHero/Scripts/Infrastructure/Services/Input/MobileInputService.cs
+++ Assets/UndeadHero/Scripts/Infrastructure/Services/Input/MobileInputService.cs
@@ -4,8 +4,12 @@ namespace UndeadHero.Infrastructure.Services.Input {
   public class MobileInputService : IInputService {
     protected const string XAxisId = "Horizontal";
     protected const string YAxisId = "Vertical";
+    protected const string AttackButtonId = "Attack";
 
     public virtual Vector2 MovementAxis =>
       new(SimpleInput.GetAxis(XAxisId), SimpleInput.GetAxis(YAxisId));
+
+    public bool AttackTriggered =>
+      SimpleInput.GetButtonUp(AttackButtonId);
   }
 }
