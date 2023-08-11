1226d331bcc4bd09b69e9b1651b076d542d4ead1
snezhniylis <noreply@snezhniylis.dev>
2023-08-11 19:10:15 +0300

    Re-organize character prefabs

--- Assets/UndeadHero/Scripts/Infrastructure/Services/AssetManagement/AssetPaths.cs
+++ Assets/UndeadHero/Scripts/Infrastructure/Services/AssetManagement/AssetPaths.cs
@@ -1,6 +1,6 @@
 namespace UndeadHero.Infrastructure.Services.AssetManagement {
   public static class AssetPaths {
-    public const string Doge = "Characters/Playable/Doge/Doge";
+    public const string Doge = "Characters/Playable/Doge";
     public const string Hud = "UI/HUD";
   }
 }
