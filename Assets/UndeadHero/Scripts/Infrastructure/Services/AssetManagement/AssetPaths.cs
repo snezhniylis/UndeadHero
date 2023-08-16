8c684bd590254995f06221348400be3015dad327
snezhniylis <noreply@snezhniylis.dev>
2023-08-16 19:41:30 +0300

    Move all scene assets to one folder

--- Assets/UndeadHero/Scripts/Infrastructure/Services/AssetManagement/AssetPaths.cs
+++ Assets/UndeadHero/Scripts/Infrastructure/Services/AssetManagement/AssetPaths.cs
@@ -1,7 +1,7 @@
 namespace UndeadHero.Infrastructure.Services.AssetManagement {
   public static class AssetPaths {
     public const string Hud = "UI/HUD";
-    public const string EnemyLootContainer = "Pickups/EnemyLootContainer";
+    public const string EnemyLootContainer = "Level/Pickups/EnemyLootContainer";
 
     public const string HeroStaticData = "StaticData/Heroes/Doge";
     public const string EnemiesStaticData = "StaticData/Enemies";
